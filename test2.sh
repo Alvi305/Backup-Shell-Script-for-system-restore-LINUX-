32dbd97587330d63a313c8eec540dd9043b484ef
Alvi305 <78257600+Alvi305@users.noreply.github.com>
2023-01-27 10:06:52 +0800

    find -exec command
    Files files and uses -exec cp --parent command to copy files with full directory path to destination folder

--- test2.sh
+++ test2.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-echo -e "This is for testing purporse"
+echo -e "This is for testing purpose"
+
+
+
 
 file="/home/alvi/Tasks/blacklist.txt"  #blacklistfile
 file2="/home/alvi/Tasks/whitelist_ext.txt" #whitelist extension text file
@@ -9,7 +12,7 @@ blk_arr=() #array for holding blacklistfiles
 white_arr=() #array for whitelist file extension
 
 
-#Read blklist dirs into array
+# Read blklist dirs into array
 while IFS= read -r line; do
 	blk_arr+=("$line")
 done < $file
@@ -17,13 +20,14 @@ done < $file
 echo -e "-------------------------------------------- "
 
 
+# print out blklist directories
 echo "blacklist dirs are:"
 
 for i in ${blk_arr[@]}; do
 	echo $i
 done
 
-#Read whitelist exts into array
+# Read whitelist exts into array
 while IFS= read -r line; do
 	white_arr+=("$line")
 done < $file2
@@ -31,6 +35,7 @@ done < $file2
 echo -e "-------------------------------------------- "
 
 
+# print out whitelist exts
 echo "whitelist extensions are:"
 
 for k in ${white_arr[@]}; do
@@ -43,49 +48,62 @@ echo -e "------------------------------------------- "
 
 echo -e "------------FILE COPY STARTS---------------"
 
-for i in  `ls -d /home/alvi/Tasks/testfolder/ImageNetModel/*`
+for i in  `ls -dR /home/alvi/Tasks/testfolder/ImageNetModel/*`
 
 do
  
 if [ -d "$i" ]
 then
- echo "$i is a directory"
+ echo -e "$i is a directory"
  
  #cd "$i"
  #echo -e "inside $i"
  
- if [ "$(ls -A $i)" ]; then
-    echo "$i is not empty"
-    fi
+ if [ "$(ls -A $i)" ]; then # check to see if dir is empty 
+    echo -e "$i is not empty"
     
-    if [[ ! " ${blk_arr[*]} " =~ " ${i} " ]]; then
+    
+    if [[ ! " ${blk_arr[*]} " =~ " ${i} " ]]; then # if  1st depth directory is not a blacklist directory then
      
-    cd "$i"
-    echo -e "inside $i"
-    fi
+    #cd "$i"
+    #echo -e "inside $i"
+    echo -e "$i is not blacklisted"
     
     
-    
-    for j in `ls -d $i/*`
+    for j in `ls -dR $i/*` # check 2nd depth of subdirectory 
     do
-    echo -e "$j"
-     if [ -f "$j" ]; then
-        for k in ${white_arr[@]};
+    echo -e " 2nd depth dir $j"
+    
+     #if [ -d "$j" ]; then
+        for k in ${white_arr[@]}; # go throu
         do
-         find "$i" -name '*$k' -execdir cp --parents  '{}' '/home/alvi/Tasks/desttest' && echo -e "COPYING $j..." ';'
+        
+          echo -e "copying files with $k extension"   
+          echo "$j"
+          find "$j" -name "*$k" -exec cp --parents {} ~/Tasks/desttest \;
+   
         done
-        fi
-     
-    done
-    cd ..
+      #fi
+      
+    done 
 
+     fi
+        
+  fi
+ 
      
+     
+    #done
+    #cd ..
+
+   
     elif [ -f "$i" ]
  then
      echo  -e "------------------------ "
      echo -e "COPYING FILE $i... "
      #cp -R "$i" /home/alvi/Tasks/desttest
-     
-fi  
+   
+fi
+       
 done 
 
