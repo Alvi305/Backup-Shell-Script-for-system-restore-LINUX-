13dd1f8d1a6d639edf967e999372a7c1f4ebf20c
Alvi305 <78257600+Alvi305@users.noreply.github.com>
2023-02-15 11:54:09 +0800

    Main Script

--- Backup_Script.sh
+++ Backup_Script.sh
@@ -4,16 +4,39 @@
 # MODULE 1 
 
 # INPUT SRC DIRECTORY TO COPY FROM  --CHANGE THIS
+
 src_dir=`ls -dR /home/alvi/TAKS/testfolder/*` 
 
 # DESTINATION DIRECTORY TO COPY FILES TO
-dest_dir="/home/alvi/TAKS/desttest"
 
- #blacklistfile   --CHANGE THIS
-blacklist_file="/home/alvi/TAKS/blacklist.txt" 
+# use relative path to script
+
+repository_script_path=$(realpath $0)
+repository_path=$(dirname $repository_script_path)
+
+dest_dir="$repository_path/Backups"
+
+
+
+
+if [ -d "$dest_dir" ];then
+	echo -e "Directory already exists:$dest_dir"
+else 
+	mkdir -p "$dest_dir"
+	echo -e "Directory created $dest_dir"
+fi
+
+
+# Using relative paths from script to access text files
+
+blacklistfile_name="/blacklist.txt"
+whitelistfile_name="/whitelist_ext.txt"
+
+ #blacklistfile   
+blacklist_file=$repository_path$blacklistfile_name
 
 #whitelist extension text file   --CHANGE THIS
-whitelist_file="/home/alvi/TAKS/whitelist_ext.txt" 
+whitelist_file=$repository_path$whitelistfile_name
 
 #array for holding blacklist directories
 black_arr=() 
@@ -58,7 +81,6 @@ done
 echo -e "------------------------------------------- "
 
 
-
 # Check Directories With Blacklist Directories
 
 echo -e "------------FILE COPY STARTS---------------"
@@ -89,7 +111,7 @@ do
              # go through whitelist extensions and copy required files while maintaining directory structure
         	for k in ${white_arr[@]};
        	     do
-          		find "$j" -name "*$k" -exec cp --parents {} $dest_dir \; # 
+          		find "$j" -name "*$k" -exec cp --parents {} $dest_dir \; # --CHANGE THIS
    
      		 done #(ends whitelist extension for loop)
      
@@ -114,7 +136,7 @@ do
         # copy files with specefic whitelist extension
      	for k in ${white_arr[@]}; 
         	do     
-          		find "$i" -type f -name "*$k" -exec cp --parents {} $dest_dir \;  # 
+          		find "$i" -type f -name "*$k" -exec cp --parents {} $dest_dir \;  # --CHANGE THIS
    
         	done
        
@@ -124,22 +146,24 @@ done
 
 
 
-
 # MODULE 2
 
 # enables storing terminal command 
 HISTFILE=~/.bash_history
 set -o history
 
-# destination directory for SystemLog backup --CHANGE THIS
+# destination directory for SystemLog backup 
 
-dest_syslog="/home/alvi/SystemLogs"
-dest_home="/home/alvi/SystemLogs/home"
-usr_local="/home/alvi/SystemLogs/usr"
-keyrings_dir="/home/alvi/SystemLogs/usr/share/keyrings"
-sys_usr_kernel="/home/alvi/SystemLogs/proc/sys/kernel"
+# change to preference
 
-# variables for text files to store system critical info --CHANGE THIS
+dest_syslog="$repository_path/SystemLogs"
+
+dest_home="/home"
+usr_local="/usr"
+keyrings_dir="/share/keyrings"
+sys_usr_kernel="/proc/sys/kernel"
+
+# variables for text files to store system critical info
 
 sys_log="/syslog.txt"
 kernel="/kernellog.txt"
@@ -149,6 +173,52 @@ snap="/snap_pkgs.txt"
 conda="/conda_pkgs.txt"
 history="/history.txt"
 
+# check if directory already exists or else create directory
+
+#dest_syslog
+
+if [ -d "$dest_syslog" ];then
+  echo "Directory already exists:$dest_syslog"
+else
+  mkdir -p "$dest_syslog"
+  echo "Directory created:$dest_syslog"
+fi
+
+#home
+
+if [ -d "$dest_syslog$dest_home" ];then
+  echo "Directory already exists:$dest_syslog$dest_home"
+else
+  mkdir -p "$dest_syslog$dest_home"
+  echo "Directory created:$dest_syslog$dest_home"
+fi
+
+#usr_local
+
+if [ -d "$dest_syslog$usr_local" ];then
+  echo "Directory already exists:$dest_syslog$usr_local"
+else
+  mkdir -p "$dest_syslog$usr_local"
+  echo "Directory created:$dest_syslog$usr_local"
+fi
+
+#keysrings
+
+if [ -d "$dest_syslog$usr_local$keyrings_dir" ];then
+  echo "Directory already exists:$dest_syslog$usr_local$keyrings_dir"
+else
+  mkdir -p "$dest_syslog$usr_local$keyrings_dir"
+  echo "Directory created:$dest_syslog$usr_local$keyrings_dir"
+fi
+
+#sys_user_kernel
+
+if [ -d "$dest_syslog$sys_usr_kernel" ];then
+  echo "Directory already exists:$dest_syslog$sys_usr_kernel"
+else
+  mkdir -p "$dest_syslog$sys_usr_kernel"
+  echo "Directory created:$dest_syslog$sys_usr_kernel"
+fi
 
 
 
@@ -159,7 +229,7 @@ journalctl --system --since "7 days ago" > $dest_syslog$sys_log
 journalctl -t kernel --since "7 days ago" > $dest_syslog$kernel
 
 
-# list apt_packages 
+# list apt_packages -- CHANGE THIS
  
   #auto installed packages  
   apt-cache pkgnames > $dest_syslog$apt_cache
@@ -189,7 +259,7 @@ rysnc -az --progress /srv $dest_syslog
 rsync -az --progress /opt $dest_syslog
 
 #/usr/local directory
-rsync -az --progress /usr/local/ $usr_local
+rsync -az --progress /usr/local/ $dest_syslog$usr_local
 
 # /boot directory
 rsync -az --progress /boot $dest_syslog
@@ -198,41 +268,41 @@ rsync -az --progress /boot $dest_syslog
 rsync -az --progress /root $dest_syslog
 
 # for CUDA public private keys
-rsync -az --progress /usr/share/keyrings $keyrings_dir
+rsync -az --progress /usr/share/keyrings $dest_syslog$usr_local$keyrings_dir
 
 #kernel
-rsync -az --progress /proc/sys/kernel $sys_usr_kernel
+rsync -az --progress /proc/sys/kernel $dest_syslog$sys_usr_kernel
 
 # files in home directory 
 
-rsync -az --progress ~/.bashrc $dest_home
+rsync -az --progress ~/.bashrc $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.bash_aliases $dest_home
+rsync -az --progress ~/.bash_aliases $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.vimrc $dest_home
+rsync -az --progress ~/.vimrc $dest_syslog$dest_home
 
 
-rsync -az --progress ~/task.rc $dest_home
+rsync -az --progress ~/task.rc $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.profile $dest_home
+rsync -az --progress ~/.profile $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.xinitrc $dest_home
+rsync -az --progress ~/.xinitrc $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.gitconfig $dest_home
+rsync -az --progress ~/.gitconfig $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.ssh/config $dest_home
+rsync -az --progress ~/.ssh/config $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.profile $dest_home
+rsync -az --progress ~/.profile $dest_syslog$dest_home
 
 
-rsync -az --progress ~/.editor $dest_home
+rsync -az --progress ~/.editor $dest_syslog$dest_home
 
 
 #stores history of terminal commands
